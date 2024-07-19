2aeb9da8b1f53e0e54dfbc9fe1fdad36c41b76ce
pauloWeslleY <weslleyspfc@13outlook.com>
2024-07-18 23:48:09 -0300

    feat: add project done

--- js/main.js
+++ js/main.js
@@ -6,31 +6,31 @@ const description = document.querySelector("#description");
 const line = document.querySelector("#line");
 
 const onScroll = (event) => {
-   const scrollPosition = event.target.scrollingElement.scrollTop;
+  const scrollPosition = event.target.scrollingElement.scrollTop;
 
-   if (scrollPosition > 150) {
-      banner.style.backgroundSize = "150%";
-      title.style.opacity = 0;
-      title.style.translate = "0 -50px";
-      title.style.scale = "0.9";
-      description.style.opacity = 0;
-      description.style.translate = "0 -40px";
-      description.style.scale = "0.8";
-      line.style.opacity = 0;
-      line.style.translate = "0 -40px";
-      line.style.scale = "0.8";
-   } else {
-      banner.style.backgroundSize = "180%";
-      title.style.opacity = 1;
-      title.style.translate = 0;
-      title.style.scale = 1;
-      description.style.opacity = 1;
-      description.style.translate = 0;
-      description.style.scale = 1;
-      line.style.opacity = 1;
-      line.style.translate = 0;
-      line.style.scale = 1;
-   }
+  if (scrollPosition > 150) {
+    banner.style.backgroundSize = "150%";
+    title.style.opacity = 0;
+    title.style.translate = "0 -50px";
+    title.style.scale = "0.9";
+    description.style.opacity = 0;
+    description.style.translate = "0 -40px";
+    description.style.scale = "0.8";
+    line.style.opacity = 0;
+    line.style.translate = "0 -40px";
+    line.style.scale = "0.8";
+  } else {
+    banner.style.backgroundSize = "180%";
+    title.style.opacity = 1;
+    title.style.translate = 0;
+    title.style.scale = 1;
+    description.style.opacity = 1;
+    description.style.translate = 0;
+    description.style.scale = 1;
+    line.style.opacity = 1;
+    line.style.translate = 0;
+    line.style.scale = 1;
+  }
 }
 
 document.addEventListener("scroll", onScroll);
